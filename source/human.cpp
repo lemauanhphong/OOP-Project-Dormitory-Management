4f4605ea71ec22a7331f6587fc06f56f7c654f1c
sinkthemall <tphat9a2@gmail.com>
2022-06-02 17:09:43 +0700

    adding setter, getter functions
    including ten, id, sdt, mssv, khoa, truong, toa, phong,

--- source/human.cpp
+++ source/human.cpp
@@ -9,7 +9,31 @@ human::~human()
 {
     //dtor
 }
-human::human(string Ten,string Id,int Tuoi,string Sdt) : ten(Ten), id(Id), tuoi(Tuoi), sdt(Sdt)
+human::human(string Ten,string Id,string Sdt) : ten(Ten), id(Id), sdt(Sdt)
 {
 
+}
+string human::getTen()
+{
+    return ten;
+}
+string human::getId()
+{
+    return  id;
+}
+string human::getSdt()
+{
+    return sdt;
+}
+void human::setTen(string a)
+{
+    ten = a;
+}
+void human::setId(string a)
+{
+    id = a;
+}
+void human::setSdt(string a)
+{
+    sdt = a;
 }
